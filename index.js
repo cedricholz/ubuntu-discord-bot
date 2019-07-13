fb93e38db240c702cc16c761a80d5726b8b43a60
Cedric Holz <cedric@dreamship.com>
2019-07-12 18:37:34 -0700

    fixed 2 week timers

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const timeWaiter = (sheet, index1, index2, nextDate, timeToWait) => {
 };
 
 
-const getDayOfWeek = (day, nextWeek, hourOfDayInMilliseconds) => {
+const getDayOfWeek = (day, evenWeek, hourOfDayInMilliseconds) => {
     // let nineHoursInMilliseconds = 32400000;
 
     let d = new Date();
@@ -45,7 +45,7 @@ const getDayOfWeek = (day, nextWeek, hourOfDayInMilliseconds) => {
         timeThing += 7
     }
 
-    if (nextWeek) {
+    if (evenWeek) {
         timeThing += 7
     }
 
@@ -55,10 +55,10 @@ const getDayOfWeek = (day, nextWeek, hourOfDayInMilliseconds) => {
 };
 
 function setTimeout_(fn, delay) {
-    var maxDelay = Math.pow(2, 31) - 1;
+    let maxDelay = Math.pow(2, 31) - 1;
 
     if (delay > maxDelay) {
-        var args = arguments;
+        let args = arguments;
         args[1] -= maxDelay;
 
         return setTimeout(function () {
@@ -90,6 +90,77 @@ const getLastDayOfMonth = (next) => {
     return new Date(d.getTime() + nineHoursInMilliseconds + timeOffset)
 };
 
+const getWeekNo = (dt) => {
+    let tdt = new Date(dt.valueOf());
+    let dayn = (dt.getDay() + 6) % 7;
+    tdt.setDate(tdt.getDate() - dayn + 3);
+    let firstThursday = tdt.valueOf();
+    tdt.setMonth(0, 1);
+    if (tdt.getDay() !== 4) {
+        tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
+    }
+    return 1 + Math.ceil((firstThursday - tdt) / 604800000);
+};
+
+
+function getMonday(d) {
+    d = new Date(d);
+    let day = d.getDay();
+    let diff = d.getDate() - day + (day === 0 ? -6 : 1);
+    return new Date(d.setDate(diff));
+}
+
+const isDateBeforeToday = (date) => {
+    return date < new Date();
+};
+
+const getDateCopyAndNextWeekIfNeeded = (daysPastMonday) => {
+
+    let returnDate = new Date();
+    returnDate.setDate(getMonday(new Date()).getDate() + daysPastMonday);
+
+    if (isDateBeforeToday(returnDate)) {
+        returnDate.setDate(returnDate.getDate() + 14);
+    }
+
+    returnDate.setHours(0, 0, 0, 0);
+    return new Date(returnDate.getTime() + tenHoursInMilliseconds + timeOffset)
+};
+
+const getTwoWeekDates = () => {
+
+
+    let tuesday1 = getDateCopyAndNextWeekIfNeeded(1);
+    let thursday1 = getDateCopyAndNextWeekIfNeeded(3);
+    let sunday1 = getDateCopyAndNextWeekIfNeeded(6);
+
+    let tuesday2 = getDateCopyAndNextWeekIfNeeded(8);
+    let thursday2 = getDateCopyAndNextWeekIfNeeded(10);
+    let sunday2 = getDateCopyAndNextWeekIfNeeded(13);
+
+    let weekNo = getWeekNo(new Date());
+
+    if (weekNo % 2 !== 0) {
+        return {
+            tuesday1,
+            thursday1,
+            sunday1,
+            tuesday2,
+            thursday2,
+            sunday2,
+        }
+    } else {
+        return {
+            tuesday1: tuesday2,
+            thursday1: thursday2,
+            sunday1: sunday2,
+            tuesday2: tuesday1,
+            thursday2: thursday1,
+            sunday2: sunday1,
+        }
+    }
+
+};
 
 client.on('ready', () => {
 
@@ -118,23 +189,25 @@ client.on('ready', () => {
     timeWaiter(sheet, 17, 19, nextFridayAT9, oneWeekInMilliseconds);
 
     // Family Dinner
+
+
+    const {
+        tuesday1,
+        thursday1,
+        sunday1,
+        tuesday2,
+        thursday2,
+        sunday2,
+    } = getTwoWeekDates();
+
     const twoWeeksInMilliseconds = 1209600000;
 
-    let nextTuesdayAT10 = getDayOfWeek(2, false, tenHoursInMilliseconds);
-    let nextThursdayAT10 = getDayOfWeek(4, false, tenHoursInMilliseconds);
-    let nextSundayAT10 = getDayOfWeek(0, false, tenHoursInMilliseconds);
-
-    let nextNextTuesdayAT10 = getDayOfWeek(2, true, tenHoursInMilliseconds);
-    let nextNextThursdayAT10 = getDayOfWeek(4, true, tenHoursInMilliseconds);
-    let nextNextSundayAT10 = getDayOfWeek(0, true, tenHoursInMilliseconds);
-
-    timeWaiter(sheet, 1, null, nextTuesdayAT10, twoWeeksInMilliseconds);
-    timeWaiter(sheet, 3, null, nextThursdayAT10, twoWeeksInMilliseconds);
-    timeWaiter(sheet, 5, null, nextSundayAT10, twoWeeksInMilliseconds);
-    timeWaiter(sheet, 7, null, nextNextTuesdayAT10, twoWeeksInMilliseconds);
-    timeWaiter(sheet, 9, null, nextNextThursdayAT10, twoWeeksInMilliseconds);
-    timeWaiter(sheet, 11, null, nextNextSundayAT10, twoWeeksInMilliseconds);
-
+    timeWaiter(sheet, 1, null, tuesday1, twoWeeksInMilliseconds);
+    timeWaiter(sheet, 3, null, thursday1, twoWeeksInMilliseconds);
+    timeWaiter(sheet, 5, null, sunday1, twoWeeksInMilliseconds);
+    timeWaiter(sheet, 7, null, tuesday2, twoWeeksInMilliseconds);
+    timeWaiter(sheet, 9, null, thursday2, twoWeeksInMilliseconds);
+    timeWaiter(sheet, 11, null, sunday2, twoWeeksInMilliseconds);
 
 });
 
